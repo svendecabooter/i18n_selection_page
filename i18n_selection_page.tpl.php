fbca69f1b3f3b6ea714137291ea5d0372dedfaae
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2011-09-09 09:59:15 +0200

    Initial release

---
+++ i18n_selection_page.tpl.php
@@ -0,0 +1,7 @@
+<p>Hello,</p>
+<p>You are coming from <?php print $data['from_link'];?></p>
+<p>You should go to:</p>
+<ul>
+  <li class="first"><?php print $data['links']['nl'];?></li>
+  <li class="last"><?php print $data['links']['fr'];?></li>
+</ul>
