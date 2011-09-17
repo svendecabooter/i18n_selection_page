1f5908bead9bb379260cefaf70a422486d78fb81
Pol Dell'aiera <pol.dellaiera@gmail.com>
2011-09-17 10:48:54 +0200

    Pass the module through Coder, enable more validation checks in the admin settings page, change the way the switch engine is working, doxygen compliance.

--- i18n_selection_page.tpl.php
+++ i18n_selection_page.tpl.php
@@ -1,3 +1,13 @@
+<?php
+/**
+ * @file
+ * The template file of the module
+ *
+ * Variables used:
+ *  - $data: array of links in each language enabled.
+ *
+ */
+?>
 <p>Hello,</p>
 <p>You are coming from <?php print $data['from_link']; ?></p>
 <p>You should go to:</p>
