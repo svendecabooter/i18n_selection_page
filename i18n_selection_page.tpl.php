92afad7319dadbecd1a5fc9bf1788e9d2ba8b865
Pol Dell'aiera <pol.dellaiera@gmail.com>
2011-09-17 02:06:32 +0200

    Fix bugs, refactoring some parts.

--- i18n_selection_page.tpl.php
+++ i18n_selection_page.tpl.php
@@ -2,7 +2,7 @@
 <p>You are coming from <?php print $data['from_link']; ?></p>
 <p>You should go to:</p>
 <ul>
-  <?php foreach($data['links'] as $links ): ?>
-    <li class="first"><?php print $links;?></li>
+  <?php foreach ($data['links'] as $links): ?>
+  <li class="first"><?php print $links;?></li>
   <?php endforeach ?>
 </ul>
