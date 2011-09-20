e0851cd3214c463bcf4fdad7cf24cbd9557328ab
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2011-09-20 12:43:54 +0200

    Updates in the tempalte, implements hook_requirements().

--- i18n_selection_page.tpl.php
+++ i18n_selection_page.tpl.php
@@ -11,8 +11,4 @@
 <p>Hello,</p>
 <p>You are coming from <?php print $data['from_link']; ?></p>
 <p>You should go to:</p>
-<ul>
-  <?php foreach ($data['links'] as $links): ?>
-  <li class="first"><?php print $links;?></li>
-  <?php endforeach ?>
-</ul>
+<?php print $data['links_html']; ?>
